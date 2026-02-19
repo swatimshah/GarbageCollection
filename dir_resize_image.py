01baf53120cc7a0e0a71b51ad75be519f2176c9b
swatimshah <swati.matwankar@gmail.com>
2026-02-19 17:40:38 +0530

    Add files via upload
    Created a list of mapping between the image and the labels. Pending: split between train and test data and then training and testing.

--- dir_resize_image.py
+++ dir_resize_image.py
@@ -4,11 +4,20 @@ import numpy as np
 from pathlib import Path
 from tensorflow.keras.preprocessing import image
 
-def downsize_image(my_input_dir, my_output_dir, quality=85):
+def count_files_os(directory_path):
+    count = 0
+    for root, dirs, files in os.walk(directory_path):
+        count = len(files)
+    return count
+
+
+def downsize_image(my_input_dir, my_output_dir, y_train, quality=85):
     """
     Resizes an image maintaining aspect ratio and compresses it.
     """
     img_batch = np.empty((0, 300, 400, 3), dtype=float)
+    count = 0
+    training_data_list = []
 
     for entry in os.listdir(my_input_dir):
         full_path = os.path.join(my_input_dir, entry)
@@ -33,11 +42,23 @@ def downsize_image(my_input_dir, my_output_dir, quality=85):
                 img_array = image.img_to_array(resized_img) / 255.0
 
                 # 2. Add batch dimension: (300, 400, 3) -> (1, 300, 400, 3)
-                temp = np.expand_dims(img_array, axis=0)
-                print(temp.shape)
-                img_batch = np.append(img_batch, temp, axis = 0)
+                img_exp = np.expand_dims(img_array, axis=0)
+                print(img_exp.shape)
+                
+                try:
+                    my_image = np.array(img_exp)
+                    # Create a dictionary for each image and its label
+                    image_data = {
+                        'image_data': my_image,
+                        'label': result[count]
+                    }
+                    training_data_list.append(image_data)
+                except IOError:
+                    print(f"Error loading image {filename}")
+               
+                count += 1 
 
-                print(img_batch.shape)
+    print(training_data_list[50]['label'])	
 
 
 # Example Usage on Redmi
@@ -45,8 +66,24 @@ def downsize_image(my_input_dir, my_output_dir, quality=85):
 input_dir = 'D:/TestData-Garbage-Collection/input'
 output_dir = 'D:/TestData-Garbage-Collection/resized'
 
+num_files_leaves = count_files_os(input_dir + "\\leaves")
+print(f"Total files : {num_files_leaves}")
+leaves = [0] * num_files_leaves
+
+num_files_paper = count_files_os(input_dir + "\\paper")
+print(f"Total files : {num_files_paper}")
+paper = [1] * num_files_paper
+
+num_files_plastic = count_files_os(input_dir + "\\plastic")
+print(f"Total files : {num_files_plastic}")
+plastic = [2] * num_files_plastic
+
+result = np.concatenate((leaves, paper, plastic), axis=0)
+print(result)
+
 
 if os.path.exists(input_dir):
-    downsize_image(input_dir, output_dir)
+    print("Start ...")
+    downsize_image(input_dir, output_dir, result)
 else:
     print("File not found.")
